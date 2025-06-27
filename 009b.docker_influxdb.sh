af8b6ee5b5fc2da6b3852970e2d4f37d730b03f4
vishnuiot <vishnuprasad.iit@gmail.com>
2025-06-27 19:18:55 +0200

    update

--- 009b.docker_influxdb.sh
+++ 009b.docker_influxdb.sh
@@ -1,7 +1,7 @@
 
 # sudo docker pull influxdb
 
-# sudo docker run -d --name=influxdbV2 -p 8086:8086 influxdb:2
+# sudo docker run -d --name=inflxdbv2.0 -p 8086:8086 influxdb:2
 
 sudo docker start influxdbV2
 # -- influxdbV2 is the name of the container
--- 009c.docker_commands.sh
+++ 009c.docker_commands.sh
@@ -1,10 +1,11 @@
 # sudo docker images -a
-
 # sudo docker ps
 # sudo docker stop 13c599907d8
 # sudo docker rmi -f 13c599907d8
 
-
+# docker update --restart=<policy> <container_id_or_name>
+sudo docker update --restart=always b374bab3a8d3
+sudo docker update --restart=always f73f5bc5ffe3
 
 # sudo systemctl start docker
 # sudo systemctl enable docker
@@ -13,6 +14,8 @@
 sudo docker update --restart=always b374bab3a8d3
 sudo docker update --restart=always f73f5bc5ffe3
 ## remove all containers
-# docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
+docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
+# # remove all images
+# sudo docker system prune
 
 
