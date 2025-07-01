bed92711a753d4897e0ee55572b6be223f48634d
vishnuiot <vishnuprasad.iit@gmail.com>
2025-07-01 18:03:11 +0200

    test

--- 009c.docker_commands.sh
+++ 009c.docker_commands.sh
@@ -19,3 +19,5 @@ docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
 # sudo docker system prune
 
 
+
+
---
+++ 009d.docker_compose.sh
@@ -0,0 +1,9 @@
+## docker compose notes
+docker compose version
+sudo docker-compose up -d
+
+sudo docker-compose ps
+docker compose stop
+docker compose down
+sudo docker-compose logs
+docker compose build
